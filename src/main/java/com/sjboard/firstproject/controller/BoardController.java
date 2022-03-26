b7d5c99409a97ac67df4388650a6b60356e42dc6
이성준 <thwn40@naver.com>
2022-03-26 21:17:22 +0900

    20220325

--- src/main/java/com/sjboard/firstproject/controller/BoardController.java
+++ src/main/java/com/sjboard/firstproject/controller/BoardController.java
@@ -3,16 +3,14 @@ package com.sjboard.firstproject.controller;
 import com.sjboard.firstproject.domain.Board;
 import com.sjboard.firstproject.dto.BoardResponseDto;
 import com.sjboard.firstproject.dto.BoardSaveDto;
+import com.sjboard.firstproject.dto.BoardUpdateDto;
 import com.sjboard.firstproject.repository.BoardRepository;
 import com.sjboard.firstproject.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.data.ConditionalOnRepositoryType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -40,7 +38,26 @@ private final BoardService boardService;
     boardService.updateView(id);
     model.addAttribute("board", board);
     return "boardView";
-
+}
+// 수정폼
+@GetMapping("/board/{id}/update")
+    public String boardUpdateForm(@PathVariable Long id, Model model) {
+    BoardResponseDto board = boardService.findById(id);
+    model.addAttribute("board", board);
+    //boardService.update(id, BoardUpdateDto.builder().title(title).content(content).build());
+   return "boardUpdate";
 }
 
+@PostMapping("/board/{id}/update")
+public String boardUpdate(@PathVariable Long id, String title, String content){
+    BoardUpdateDto board = BoardUpdateDto.builder().title(title).content(content).build();
+    boardService.update(id,board);
+    return "redirect:/board/{id}";
 }
+@DeleteMapping("/board/{id}")
+    public String boardDelete(@PathVariable Long id){
+    boardService.deleteById(id);
+    return "redirect:/board";
+}
+}
+
