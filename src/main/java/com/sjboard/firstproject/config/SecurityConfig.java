cdb1ce57627d35193ad55bb6b72128d77584df82
thwn40 <62760166+thwn40@users.noreply.github.com>
2022-03-27 00:25:58 +0900

    20220327

---
+++ src/main/java/com/sjboard/firstproject/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.sjboard.firstproject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll() // 위에 세개말고 아무나 들어갈수있음
+                .and()
+                .formLogin()
+                .loginPage("/login"); //로ㅓ그인이 안되어있으면 로그인페이지로 보낸다.
+    }
+}
--- src/main/java/com/sjboard/firstproject/controller/HomeController.java
+++ src/main/java/com/sjboard/firstproject/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.sjboard.firstproject.controller;
 
 import com.sjboard.firstproject.domain.Board;
+import com.sjboard.firstproject.domain.Member;
 import com.sjboard.firstproject.dto.BoardResponseDto;
 import com.sjboard.firstproject.repository.BoardRepository;
 import com.sjboard.firstproject.service.BoardService;
@@ -8,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -21,4 +24,37 @@ public class HomeController {
         model.addAttribute("board", board);
         return "home";
     }
+
+    @GetMapping("/user")
+    public @ResponseBody String user(){
+        return "user";
+    }
+    @GetMapping("/admin")
+    public @ResponseBody String admin(){
+        return "admin";
+    }
+    @GetMapping("/manager")
+    public @ResponseBody String manager(){
+        return "manager";
+    }
+    @GetMapping("/loginForm")
+    public String loginForm(){
+        return "loginForm";
+    }
+    @GetMapping("/joinForm")
+    public String joinForm(){
+        return "joinForm";
+    }
+    @PostMapping("/join")
+    public @ResponseBody String join(Member member){
+        System.out.println(member);
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc(){
+        return "회원가입 완료";
+    }
+
+
 }
