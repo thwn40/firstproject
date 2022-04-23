ffcb34abca129f1f1a5255d007c1f43474b45392
thwn40 <62760166+thwn40@users.noreply.github.com>
2022-04-23 18:54:38 +0900

    20220423 githubactions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,11 @@
-#!/bin/ bash
+#!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=firstproject
+
+echo "> build 파일 복사"
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl action firstproject | grep java | awk '{print $1}')
